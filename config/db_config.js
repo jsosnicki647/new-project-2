8a5b61690df3c6b7732dd3cdba11ca866dcf7c7e
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-10-10 21:12:01 -0400

    good grief

--- config/db_config.js
+++ config/db_config.js
@@ -1,6 +1,10 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
