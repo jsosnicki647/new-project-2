02649c5855ed41ff6b1e928d094e6fe7dc649aff
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-10-28 18:36:53 -0400

    deploy

--- public/js/auth.js
+++ public/js/auth.js
@@ -24,7 +24,7 @@ $(document).ready(() => {
                 .then((cred) => {
                     const id = cred.user.uid
                     console.log(cred)
-                    login(id)
+                    // login(id)
                 })
         }
     })
